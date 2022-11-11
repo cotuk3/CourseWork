725e4be229e5bb5fac3c8634d59e1eb58fe6d86d
cotuk3 <bohdanliashenko2004@gmail.com>
2022-11-11 09:35:17 +0200

    Updated unit tests

--- BusinessLogicLayerTests/InteractionTests.cs
+++ BusinessLogicLayerTests/InteractionTests.cs
@@ -53,12 +53,6 @@ public class InteractionTests
         inter = new();
     }
 
-    [TestMethod()]
-    public void InteractionTest1()
-    {
-
-    }
-
     #region IsQuestionValid
     [TestMethod()]
     public void IsQuestionValid_Success()
@@ -109,7 +103,7 @@ public class InteractionTests
     }
 
     [TestMethod()]
-    public void IsIndexValid_Minus1_Fail()
+    public void IsIndexValid_Negative1_Fail()
     {
         //arrange
         int index = -1;
@@ -122,7 +116,7 @@ public class InteractionTests
     }
 
     [TestMethod()]
-    public void IsIndexValid_3_Fail()
+    public void IsIndexValid_6_Fail()
     {
         //arrange
         int index = 6;
@@ -140,25 +134,13 @@ public class InteractionTests
     [TestMethod()]
     public void Add_1Test_Success()
     {
-        //act
-
-
-        inter.AddTest(test);
-    }
-
-    [TestMethod()]
-    public void Add_2Tests_Success()
-    {
         //arrange
-        Test expected = new Test();
-        expected.AddRange(test.Questions);
-        expected.AddRange(test.Questions);
-
+        var expected = Interaction.DefTest;
         //act
-
-
         inter.AddTest(test);
-        inter.AddTest(test);
+        var actual = inter.GetTest();
+        //assert
+        Assert.AreEqual(expected, actual);
     }
     #endregion
 
@@ -188,19 +170,14 @@ public class InteractionTests
 
         //act
         inter = new("file1123.xml");
-        var actual = inter.GetTest();
+        inter.GetTest();
 
-
-        //Assert.AreEqual(expected, actual, actual);
     }
 
     [TestMethod()]
     [ExpectedException(typeof(InvalidOperationException))]
     public void GetTest_FileWithOtherData_Fail()
     {
-        //arrange
-        var expected = test;
-
         //act
         File.Create("file2.xml").Close();
         var sw = File.OpenWrite("file2.xml");
@@ -208,9 +185,7 @@ public class InteractionTests
         sw.Close();
 
         inter = new("file2.xml");
-        var actual = inter.GetTest();
-
-        //Assert.AreEqual(expected, actual, actual);
+        inter.GetTest();
     }
     #endregion
 
@@ -222,10 +197,7 @@ public class InteractionTests
         Test expected = new() { Questions = { new Question("How are you?") } };
 
         //act
-
-
-
-        inter.AddQuestion(new Question("How are you?"));
+        inter.AddQuestion(Interaction.CreateQuestion("How are you?"));
         var actual = inter.GetTest();
 
         //arrange
@@ -246,8 +218,6 @@ public class InteractionTests
         expected.RemoveAt(1);
 
         //act
-
-
         inter.AddTest(test);
         inter.DeleteQuestion(1);
         var actual = inter.GetTest();
@@ -292,8 +262,6 @@ public class InteractionTests
         expected[0].Answers = question.Answers;
 
         //act
-
-
         inter.AddTest(test);
         inter.ChangeQuestion(0, question1);
         var actual = inter.GetTest();
@@ -304,12 +272,14 @@ public class InteractionTests
 
     [TestMethod()]
     [ExpectedException(typeof(QuestionException))]
-    public void ChangeQuestion_WrongIndex_Fail()
+    [DataRow(-1)]
+    [DataRow(1000)]
+    [DataRow(3)]
+    public void ChangeQuestion_WrongIndex_Fail(int index)
     {
         //act      
-
         inter.AddTest(test);
-        inter.ChangeQuestion(-1, question1);
+        inter.ChangeQuestion(index, question1);
     }
 
     [TestMethod()]
@@ -346,7 +316,7 @@ public class InteractionTests
     public void CreateQuestion_Fail(string question)
     {
         //act
-        var actual = Interaction.CreateQuestion(question);
+        Interaction.CreateQuestion(question);
     }
     #endregion
 
@@ -358,15 +328,11 @@ public class InteractionTests
         Question expectedQ = new("How are you?");
         expectedQ.Answers.Add("D");
         Test expected = new() { Questions = { expectedQ } };
-
-
         //act
-
         var actualQ = Interaction.CreateQuestion(question);
         inter.AddQuestion(actualQ);
         inter.AddAnswer(0, "D");
         var actual = inter.GetTest();
-
         //assert
         Assert.AreEqual(expected, actual);
     }
@@ -380,15 +346,11 @@ public class InteractionTests
         expectedQ.Answers.Add("D");
         Test expected = new() { Questions = { expectedQ } };
 
-
         //act
         inter.AddQuestion(question);
         inter.AddAnswer(0, "D");
         inter.AddAnswer(0, "E");
         var actual = inter.GetTest();
-
-        //assert
-        Assert.AreEqual(expected, actual, $"{actual} != {expected}");
     }
 
     [TestMethod()]
@@ -426,10 +388,13 @@ public class InteractionTests
 
     [ExpectedException(typeof(QuestionException))]
     [TestMethod()]
-    public void DeleteAnswer_NotValidQuestionIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(3)]
+    public void DeleteAnswer_NotValidQuestionIndex_Fail(int questionIndex)
     {
         //arrange
-        int questionIndex = 10, answerIndex = 0;
+        int answerIndex = 0;
 
         //act 
         inter.AddTest(test);
@@ -438,10 +403,13 @@ public class InteractionTests
 
     [ExpectedException(typeof(AnswerException))]
     [TestMethod()]
-    public void DeleteAnswer_NotValidAnswerIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(4)]
+    public void DeleteAnswer_NotValidAnswerIndex_Fail(int answerIndex)
     {
         //arrange
-        int questionIndex = 0, answerIndex = 10;
+        int questionIndex = 0;
 
         //act 
         inter.AddTest(test);
@@ -457,14 +425,12 @@ public class InteractionTests
         Test expectedT = new();
         expectedT.AddRange(test.Questions);
         expectedT[0].RightAnswer = 2;
-        var expected = expectedT.Questions[0].Answers.RightAnswer;
+        var expected = expectedT.Questions[0].RightAnswer;
 
         //act
-        inter = new("file1.xml");
-
         inter.AddTest(test);
         inter.SetRightAnswer(0, 2);
-        var actual = inter.GetTest().Questions[0].Answers.RightAnswer;
+        var actual = inter.GetTest().Questions[0].RightAnswer;
 
         //assert
         Assert.AreEqual(expected, actual);
@@ -472,37 +438,44 @@ public class InteractionTests
 
     [TestMethod()]
     [ExpectedException(typeof(AnswerException))]
-    public void SetRightAnswer_NotValidAnswerIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(4)]
+    public void SetRightAnswer_NotValidAnswerIndex_Fail(int answerIndex)
     {
+        //arrange
+        int questionIndex = 0;
         //act
         inter.AddTest(test);
-        inter.SetRightAnswer(0, 4);
+        inter.SetRightAnswer(questionIndex, answerIndex);
     }
 
     [TestMethod()]
     [ExpectedException(typeof(QuestionException))]
-    public void SetRightAnswer_NotValidQuestionIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(3)]
+    public void SetRightAnswer_NotValidQuestionIndex_Fail(int questionIndex)
     {
+        //arrange
+        int answerIndex = 0;
         //act
         inter.AddTest(test);
-        inter.SetRightAnswer(-1, 1);
+        inter.SetRightAnswer(questionIndex, answerIndex);
     }
     #endregion
 
     #region ChangeAnswer
     [TestMethod()]
-    public void ChangeAnswer_Success()
+    public void ChangeAnswer_D_Success()
     {
         //arrange 
-        Question expectedQ = new("How are you?");
-        expectedQ.Answers.Add("A");
-        Test expected = new() { Questions = { expectedQ } };
+        Test expected = new() { Questions = { question } };
+        expected.Questions[0].Answers[2] = "D";
 
         //act
-        var actualQ = Interaction.CreateQuestion(question);
-        inter.AddQuestion(actualQ);
-        inter.AddAnswer(0, "D");
-        inter.ChangeAnswer(0, 0, "A");
+        inter.AddQuestion(question);
+        inter.ChangeAnswer(0, 2, "D");
         var actual = inter.GetTest();
 
         //assert
@@ -511,20 +484,30 @@ public class InteractionTests
 
     [ExpectedException(typeof(QuestionException))]
     [TestMethod()]
-    public void ChangeAnswer_NotValidQuestionIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(3)]
+    public void ChangeAnswer_NotValidQuestionIndex_Fail(int questionIndex)
     {
+        //arrange
+        int answerIndex = 0;
         //act
         inter.AddTest(test);
-        inter.ChangeAnswer(10, 0, "Q");
+        inter.ChangeAnswer(questionIndex, answerIndex, "Q");
     }
 
     [ExpectedException(typeof(AnswerException))]
     [TestMethod()]
-    public void ChangeAnswer_NotValidAnswerIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(4)]
+    public void ChangeAnswer_NotValidAnswerIndex_Fail(int answerIndex)
     {
+        //arrange
+        int questionIndex = 0;
         //act
         inter.AddTest(test);
-        inter.ChangeAnswer(0, 10, "Q");
+        inter.ChangeAnswer(questionIndex, answerIndex, "Q");
     }
 
     #endregion
@@ -545,14 +528,12 @@ public class InteractionTests
     }
 
     [TestMethod()]
-    public void Count_Negative1_Success()
+    public void Count_EmptyTestNegative1_Success()
     {
         //arrange
         int expected = -1;
-
         //act
         int actual = inter.Count;
-
         //assert
         Assert.AreEqual(expected, actual);
     }
@@ -561,18 +542,17 @@ public class InteractionTests
     #region FilePath
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
-    public void SetFilePath_WrongExtension_Fail()
+    [DataRow("file.jpg")]
+    [DataRow("file.txt")]
+    [DataRow("file.doc")]
+    public void SetFilePath_WrongExtension_Fail(string filePath)
     {
-        //arrange 
-        string filePath = "file.jpg";
-
         //act
         inter.FilePath = filePath;
-
     }
 
     [TestMethod]
-    public void GetFilePath_Success()
+    public void GetFilePath_filexml_Success()
     {
         //arrange 
         string expected = "file.xml";
@@ -582,13 +562,12 @@ public class InteractionTests
 
         //assert
         Assert.AreEqual(expected, actual);
-
     }
     #endregion
 
     #region ResetRightAnswer
     [TestMethod()]
-    public void ResetRightAnswer_Success()
+    public void ResetRightAnswer_Null_Success()
     {
         //act
         inter.AddTest(test);
@@ -597,16 +576,18 @@ public class InteractionTests
         var rightAnswer = question.RightAnswer;
 
         Assert.IsNull(rightAnswer);
-
     }
 
     [TestMethod()]
     [ExpectedException(typeof(QuestionException))]
-    public void ResetRightAnswer_WrongQuestionIndex_Fail()
+    [DataRow(10)]
+    [DataRow(-1)]
+    [DataRow(3)]
+    public void ResetRightAnswer_WrongQuestionIndex_Fail(int questionIndex)
     {
         //act
         inter.AddTest(test);
-        inter.ReserRightAnswer(10);
+        inter.ReserRightAnswer(questionIndex);
     }
     #endregion
 
@@ -627,12 +608,14 @@ public class InteractionTests
 
     [TestMethod()]
     [ExpectedException(typeof(UserException))]
-    public void CreateUser_WrongNameandLastName_Fail()
+    [DataRow("bohdan", "liashenko")]
+    [DataRow("1Bohdan", "Li1ashenko")]
+    [DataRow("BoHdan", "L@iashenko")]
+    [DataRow("Bohdan!", "Liаshenko")]
+    public void CreateUser_WrongNameandLastName_Fail(string firstName, string lastName)
     {
-        //arrange
-        string firstName = "bohdan", lastName = "1Liashenko";
         //act
-        User actual = Interaction.CreateUser(firstName, lastName);
+        Interaction.CreateUser(firstName, lastName);
     }
 
     [TestMethod()]
@@ -642,7 +625,7 @@ public class InteractionTests
         //arrange
         string firstName = "Bohdan d", lastName = "Liashenko";
         //act
-        User actual = Interaction.CreateUser(firstName, lastName);
+        Interaction.CreateUser(firstName, lastName);
     }
 
     [TestMethod()]
@@ -650,9 +633,9 @@ public class InteractionTests
     public void CreateUser_WrongLastName_Fail()
     {
         //arrange
-        string firstName = "Bohdan", lastName = "Liashenko1";
+        string firstName = "Bohdan", lastName = "Liashenko d";
         //act
-        User actual = Interaction.CreateUser(firstName, lastName);
+        Interaction.CreateUser(firstName, lastName);
     }
 
     #endregion
@@ -674,7 +657,7 @@ public class InteractionTests
     }
 
     [TestMethod()]
-    public void GetNotExistingRightAnswers_All_Success()
+    public void GetNotExistingRightAnswers_3_Success()
     {
         //arrange
         int expected = 3;
@@ -703,6 +686,7 @@ public class InteractionTests
         Assert.IsNull(actual);
     }
 
+    [TestMethod()]
     public void CheckForRightAnswers_NotNull_Success()
     {
         //act
@@ -711,10 +695,11 @@ public class InteractionTests
             inter.ReserRightAnswer(i);
 
         string actual = inter.CheckForRightAnswers();
+        Debug.WriteLine(actual);
+
         //assert
         Assert.IsNotNull(actual);
     }
-
     #endregion
 
     [TestMethod()]
@@ -739,8 +724,6 @@ public class InteractionTests
         }
         //assert
         CollectionAssert.AreEqual(expected, actual);
-
-
     }
 
     [TestMethod()]
@@ -749,11 +732,15 @@ public class InteractionTests
         //arrange
         DateTime time = new(2022, 11, 8);
         User user = new("Bohdan", "Liashenko");
-        test[0].UserAnswer = 2;
-        test[1].UserAnswer = 0;
-        test[2].UserAnswer = 1;
-        Statistic stats = new();
-        stats.Add(user, new Mark(100, time));
+
+        test[0].UserAnswer = test[0].RightAnswer;
+        test[1].UserAnswer = test[1].RightAnswer;
+        test[2].UserAnswer = test[2].RightAnswer;
+
+        Statistic stats = new()
+        {
+            { user, new Mark(100, time) }
+        };
         string expected = stats[0];
 
         //act
